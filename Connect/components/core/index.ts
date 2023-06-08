d423271f08f4234ce5926f9714807dbc2247739c
zacharywalsh13 <zacharywalsh13@gmail.com>
2023-06-08 18:06:29 -0500

    To test expo html

--- Connect/components/core/index.ts
+++ Connect/components/core/index.ts
@@ -1,4 +1,5 @@
 export * from './GluestackUIProvider';
 export * from './Heading';
+export * from './Hstack';
 export * from './Vstack';
 export * from './styled';
--- Connect/screens/Login.tsx
+++ Connect/screens/Login.tsx
@@ -1,19 +1,36 @@
 import {SafeAreaView, Text, View} from 'react-native';
 
-import { Heading, VStack } from '../components';
+import { Heading, VStack, HStack } from '../components';
 
 import  FormTemplate from '../components/login/Forms';
 
 const Login = () => {
   return (
     <SafeAreaView>
-        <VStack>
-            <Heading>Login</Heading>
-            <FormTemplate />
-        </VStack>
+        <View style={styles.container}>
+            <VStack>
+
+                <HStack justifyContent='flex-start' style={styles.hstack}>
+                <Heading size={"xl"}>Time to get Connected </Heading>
+                </HStack>
+
+                <FormTemplate />
+            </VStack>
+        </View>
     </SafeAreaView>
   );
 };
+
+const styles = {
+    container: {
+        width: '90%',
+        backgroundColor: 'red',
+        alignSelf: 'center',
+    },
+    hstack: {
+        width: '100%',
+    }
+}
 
 export default Login;
 
