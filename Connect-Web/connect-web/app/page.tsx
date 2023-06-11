5c5459cdcf6d26743df49cb4c171acc55d5ed5c5
zacharywalsh13 <zacharywalsh13@gmail.com>
2023-06-11 18:04:19 -0500

    Added Website

---
+++ Connect-Web/connect-web/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from 'react';
+import Landing from '../components/Home/Landing/page'
+import Features from '@/components/Home/Features/page';
+
+const HomePage = () => {
+  return (
+    <>  
+      <Landing />
+    </>
+  );
+}
+
+export default HomePage;
